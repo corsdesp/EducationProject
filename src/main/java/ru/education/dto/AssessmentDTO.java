6569b2f23121bef1966179f62017620b76d508c0
Dmitrii Churovoi <corsar.k750i@gmail.com>
2017-10-29 22:00:25 +0300

    добавлены классы dto

---
+++ src/main/java/ru/education/dto/AssessmentDTO.java
@@ -0,0 +1,27 @@
+package ru.education.dto;
+
+public class AssessmentDTO {
+    private long id;
+    private String name;
+
+    public AssessmentDTO(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/education/dto/SummaryTableDTO.java
@@ -0,0 +1,57 @@
+package ru.education.dto;
+
+public class SummaryTableDTO {
+    private long id;
+    private long studentId;
+    private long assessmentId;
+    private long topicId;
+    private String value;
+
+    public SummaryTableDTO(long id, long studentId, long assessmentId, long topicId, String value) {
+        this.id = id;
+        this.studentId = studentId;
+        this.assessmentId = assessmentId;
+        this.topicId = topicId;
+        this.value = value;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public long getAssessmentId() {
+        return assessmentId;
+    }
+
+    public void setAssessmentId(long assessmentId) {
+        this.assessmentId = assessmentId;
+    }
+
+    public long getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(long topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
