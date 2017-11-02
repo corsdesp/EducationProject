e4d98e57099e2784705c4541a2cc7aff30d50fb6
corsdesp <corsar.k750i@gmail.com>
2017-11-02 04:53:14 +0300

    добавлен функционал теста, исправлены некоторые значения

--- src/test/java/ru/education/connection/QueryExecutionTest.java
+++ src/test/java/ru/education/connection/QueryExecutionTest.java
@@ -1,11 +1,16 @@
 package ru.education.connection;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.ResultSet;
 
 public class QueryExecutionTest {
 
     @Test
     public void QueryExecutionSimpleTest() {
-
+        QueryExecution queryExecution = new QueryExecution();
+        ResultSet resultSet = queryExecution.result("Topic");
+        Assert.assertNotNull(resultSet);
     }
 }
