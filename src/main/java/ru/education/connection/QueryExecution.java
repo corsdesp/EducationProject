cdaa9a7e8dccfbb6c99be1f57d3ee27dd377b464
Dmitrii Churovoi <corsar.k750i@gmail.com>
2017-11-04 00:33:31 +0300

    добавлено считывание полей с полученного результата от sql запроса

--- src/main/java/ru/education/connection/QueryExecution.java
+++ src/main/java/ru/education/connection/QueryExecution.java
@@ -1,28 +1,37 @@
 package ru.education.connection;
 
+import ru.education.entities.BaseEntity;
+import ru.education.entities.Student;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueryExecution {
 
-    public ResultSet result(String dbName) {
+    public List<BaseEntity> result() {
         Connection connection = null;
         PreparedStatement stmt = null;
         ResultSet resultSet = null;
+        List<BaseEntity> baseEntities = new ArrayList<>();
 
         try {
             DataSource ds = ConnectionPool.setUp();
             connection = ds.getConnection();
-            stmt = connection.prepareStatement("SELECT * FROM ?");
-            stmt.setString(1, dbName);
+            stmt = connection.prepareStatement("SELECT * FROM Student");
             resultSet = stmt.executeQuery();
-            return resultSet;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+
+            while (resultSet.next()) {
+                baseEntities.add(new Student(resultSet.getLong("id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name")));
+            }
+            return baseEntities;
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         } finally {
             try {
@@ -39,6 +48,6 @@ public class QueryExecution {
                 e.printStackTrace();
             }
         }
-        return null;
+        return baseEntities;
     }
 }
