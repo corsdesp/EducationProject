4b9c4dcf16361d322f05f1e4f6b1e1700bb32db6
Dmitry Churovoy <corsar.k750i@gmail.com>
2017-11-29 22:47:57 +0300

    из MainServlet убрана инициализация dataSource

--- src/main/java/ru/education/servlets/MainServlet.java
+++ src/main/java/ru/education/servlets/MainServlet.java
@@ -1,23 +1,18 @@
 package ru.education.servlets;
 
-import ru.education.connection.ConnectionPool;
-import ru.education.connection.PropertyInitializer;
 import ru.education.entities.Student;
-import ru.education.service.StudentImpl;
+import ru.education.service.StudentDaoImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 public class MainServlet extends HttpServlet {
-    private static final String PROPERTY = "postgreSql.properties";
-    private StudentImpl studentService;
+    private StudentDaoImpl studentService;
 
     private List<Student> studentList = new ArrayList<>();
 
@@ -31,7 +26,7 @@ public class MainServlet extends HttpServlet {
         String firstName = req.getParameter("first");
         String lastName = req.getParameter("second");
 
-        studentService.save(new Student(studentList.size() + 1,firstName, lastName));
+        studentService.save(new Student(studentList.size() + 1, firstName, lastName));
 
         commonParam(req, resp);
     }
@@ -48,17 +43,6 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init();
-        PropertyInitializer propertyInitializer = new PropertyInitializer();
-        Properties prop = propertyInitializer.initialize(PROPERTY);
-
-        DataSource dataSource = null;
-        try {
-            dataSource = ConnectionPool.getDataSource(prop);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        studentService = new StudentImpl(dataSource);
+        studentService = (StudentDaoImpl) getServletContext().getAttribute("studentService");
     }
 }
